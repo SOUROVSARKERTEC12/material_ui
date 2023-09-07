141fe032fcc150894731600ee419e1d60a31de0e
Admin_laravel <sourovsarker005@gmail.com>
2023-09-07 16:14:05 +0600

    Toggle Button

--- ReactMUI/src/components/Button/MuiButton.jsx
+++ ReactMUI/src/components/Button/MuiButton.jsx
@@ -1,7 +1,18 @@
-import {Button, IconButton, Stack} from '@mui/material';
+import {Button, ButtonGroup, IconButton, Stack, ToggleButton, ToggleButtonGroup} from '@mui/material';
 import SendIcon from '@mui/icons-material/Send';
+import FormatBoldIcon from '@mui/icons-material/FormatBold'
+import FormatItalicIcon from '@mui/icons-material/FormatItalic'
+import FormatUnderlinedIcon from '@mui/icons-material/FormatUnderlined'
+import React, {useState} from 'react';
 
 export const MuiButton = () => {
+    const [formats, setFormats] = React.useState([]);
+    console.log(formats)
+
+    const handleFormat = (event, newFormats) => {
+        setFormats(newFormats);
+    };
+
     return (
         <Stack spacing={4}>
             <Stack spacing={2} direction='row'>
@@ -24,11 +35,40 @@ export const MuiButton = () => {
                 <Button variant="contained" size="large">Large</Button>
             </Stack>
             <Stack spacing={2} direction='row'>
-                <Button variant="contained" size='large' startIcon={<SendIcon/>} onClick={()=> alert("Clicked")} disableRipple>Send1</Button>
+                <Button variant="contained" size='large' startIcon={<SendIcon/>} onClick={() => alert("Clicked")}
+                        disableRipple>Send1</Button>
                 <Button variant="contained" size='large' endIcon={<SendIcon/>} disableElevation>Send2</Button>
                 <IconButton aria-label="send" color="success" size="small">
                     <SendIcon/>
                 </IconButton>
+            </Stack>
+            <Stack direction='row'>
+                <ButtonGroup
+                    variant="outlined"
+                    orientation="vertical"
+                    size="small"
+                    color="warning"
+                    aria-label='alignment button group'
+                >
+                    <Button onClick={()=> alert('left clicked')}>Left</Button>
+                    <Button>Center</Button>
+                    <Button>Right</Button>
+                </ButtonGroup>
+            </Stack>
+            <Stack direction="row">
+                <ToggleButtonGroup
+                    aria-label='text formats'
+                    value={formats}
+                    exclusive
+                    onChange={handleFormat}
+                    size="large"
+                    color="error"
+                    orientation="vertical"
+                >
+                    <ToggleButton value="bold" aria-label="bold"><FormatBoldIcon/></ToggleButton>
+                    <ToggleButton value="italic" aria-label="italic"><FormatItalicIcon/></ToggleButton>
+                    <ToggleButton value='underlined' aria-label='underlined'><FormatUnderlinedIcon/></ToggleButton>
+                </ToggleButtonGroup>
             </Stack>
         </Stack>
     )
