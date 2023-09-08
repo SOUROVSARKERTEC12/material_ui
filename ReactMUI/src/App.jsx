b3e7344172ac9871e57c0803a29942c0de7b8645
Admin_laravel <sourovsarker005@gmail.com>
2023-09-08 15:34:25 +0600

    MuiSwitch

--- ReactMUI/src/App.jsx
+++ ReactMUI/src/App.jsx
@@ -4,6 +4,7 @@ import {MuiTextField} from "./components/TextField/MuiTextField.jsx";
 import {MuiSelect} from "./components/Select/MuiSelect.jsx";
 import {MuiRadioButton} from "./components/RadioButton/MuiRadioButton.jsx";
 import {MuiCheckbox} from "./components/Checkbox/MuiCheckbox.jsx";
+import {MuiSwitch} from "./components/Switch/MuiSwitch.jsx";
 
 function App() {
 
@@ -13,7 +14,8 @@ function App() {
             {/*<MuiTextField/>*/}
             {/*<MuiSelect/>*/}
             {/*<MuiRadioButton/>*/}
-            <MuiCheckbox/>
+            {/*<MuiCheckbox/>*/}
+            <MuiSwitch/>
         </>
     )
 }
