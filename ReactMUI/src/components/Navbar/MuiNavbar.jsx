c189f2538482cdea6b97e5bf9267f54673633eec
Admin_laravel <sourovsarker005@gmail.com>
2023-09-10 01:30:50 +0600

    MuiMenu

--- ReactMUI/src/components/Navbar/MuiNavbar.jsx
+++ ReactMUI/src/components/Navbar/MuiNavbar.jsx
@@ -1,8 +1,17 @@
-import {AppBar, IconButton, Stack, Toolbar, Typography} from "@mui/material";
+import {AppBar, IconButton, Menu, MenuItem, Stack, Toolbar, Typography} from "@mui/material";
 import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
 import Button from "@mui/material/Button";
-
+import {useState} from "react";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 export const MuiNavbar = () => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
         <AppBar position="static">
             <Toolbar>
@@ -16,8 +25,30 @@ export const MuiNavbar = () => {
                     <Button color='inherit'>Features</Button>
                     <Button color='inherit'>Pricing</Button>
                     <Button color='inherit'>About</Button>
+                    <Button color='inherit'
+                            id='resources-button'
+                            aria-controls={open ? 'resources-menu' : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={open ? 'true' : undefined}
+                            onClick={handleClick}
+                            endIcon={<KeyboardArrowDownIcon/>}
+                    >
+                        Resources
+                    </Button>
                     <Button color='inherit'>Login</Button>
                 </Stack>
+                <Menu
+                    id='resources-menu'
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                        'aria-labelledby': 'resources-button',
+                    }}
+                >
+                    <MenuItem onClick={handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={handleClose}>My account</MenuItem>
+                </Menu>
             </Toolbar>
         </AppBar>
     )
